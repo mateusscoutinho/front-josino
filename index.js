304282d9162cc7083afef9c2f02922de2b903a8a
mateusscoutinho <mscouto12@gmail.com>
2023-09-21 11:59:47 -0300

    aula 21/09

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ document.getElementById('botao')
 .addEventListener("click", function(){
     console.log('teste de click')
 
-   let nome= document.getElementById('nome-pokemon').value
+   let nome = document.getElementById('nome-pokemon').value
 
-    fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+   //fetch('https://pokeapi.co/api/v2/pokemon/nidoking')
+     fetch('https://pokeapi.co/api/v2/pokemon/'+nome)
     .then (function(response){
         return response.json()
     }).then (function(response){
+
+        //console.log(response.abilities)
+
+        let html_habilidades=" "
+        for(const hab of response.abilities) {
+            html_habilidades+= '<li>'+hab.ability.name+'</li>'
+        }
+        let lista=document.getElementById('habilidades')
+        lista.innerHTML=html_habilidades
+        
         console.log(response.sprites.front_default)
 
         let url= response.sprites.front_default
